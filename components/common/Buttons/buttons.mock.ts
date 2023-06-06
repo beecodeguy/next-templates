26a1c5aeeb45641f3aaf34a4056d7773ffa6bfcc
okur <bijay.okur@gmail.com>
2023-06-06 10:35:29 +0545

    build: button variants added

--- components/common/Buttons/buttons.mock.ts
+++ components/common/Buttons/buttons.mock.ts
@@ -2,7 +2,6 @@ import { IBaseButtonProps } from "components/common/Buttons/Buttons";
 
 const baseExampleString: IBaseButtonProps = {
   btnText: "Hello World",
-  backgroundColor: "coral",
 };
 
 export const baseTemplateProps = {
