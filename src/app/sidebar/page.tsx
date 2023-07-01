d1575904829544da10d0dc3f3cec1386bc27a715
okur <bijay.okur@gmail.com>
2023-07-01 14:04:55 +0545

    feature: sidebar on next tailwind working

---
+++ src/app/sidebar/page.tsx
@@ -0,0 +1,13 @@
+import Sidebar from "components/Sidebar/Sidebar";
+import TopNav from "components/TopNav/TopNav";
+
+const page = () => {
+  return (
+    <div>
+      <TopNav />
+      <Sidebar />
+    </div>
+  );
+};
+
+export default page;
